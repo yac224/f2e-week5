2aad93777429df0d57121cae526fd88c020cf438
yac224 <alex@yacoding.com>
2018-07-04 17:08:43 +0800

    Add comic detail page

---
+++ src/components/ScoreStar/index.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'scoreStar',
+  props: {
+    score: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    scoreClass() {
+      let num = this.score > 5 ? 5 : this.score < 0 ? 0 : this.score
+      return 'star-' + num
+    }
+  }
+}
+</script>
+
+<template src="./template.html"></template>
+<style lang="scss" src="./style.scss" scoped></style>
--- src/pages/ComicDetail/index.vue
+++ src/pages/ComicDetail/index.vue
@@ -1,6 +1,30 @@
 <script>
+import axios from 'axios'
+import ScoreStar from '@/components/ScoreStar'
 export default {
-  name: 'comicDetail'
+  name: 'comicDetail',
+  data() {
+    return {
+      detail: null
+    }
+  },
+  mounted() {
+    axios
+      .get('/api/detail/' + this.$route.params.id)
+      .then(res => {
+        if (res.data.chapters) {
+          this.detail = res.data
+        } else {
+          this.$router.replace('/comics')
+        }
+      })
+      .catch(() => {
+        this.$router.replace('/comics')
+      })
+  },
+  components: {
+    ScoreStar
+  }
 }
 </script>
 
