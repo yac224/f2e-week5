445f6273c2ea8fe161ba0e0fb69aa228587e713c
yac224 <alex@yacoding.com>
2018-07-04 16:07:56 +0800

    Modify router & Add pages

---
+++ src/pages/ComicDetail/index.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+  name: 'comicDetail'
+}
+</script>
+
+<template src="./template.html"></template>
+<style lang="scss" src="./style.scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/comics',
+      name: 'Comics',
+      component: () => import('@/pages/Comics')
+    },
+    {
+      path: '/comics/:id',
+      name: 'ComicDetail',
+      component: () => import('@/pages/ComicDetail')
+    },
+    {
+      path: '/comics/:id/chapter/:cid',
+      name: 'ComicChapter',
+      component: () => import('@/pages/ComicChapter')
+    },
+    {
+      path: '*',
+      redirect: '/comics'
     }
   ]
 })
